7f4756fbc03ad6708581f43d73a89a6fdc3aa437
Fe <54954812+febreno@users.noreply.github.com>
2022-09-15 13:59:34 -0300

    Update SamsungX.java

--- Exercise/TV/SamsungX.java
+++ Exercise/TV/SamsungX.java
@@ -1,5 +1,43 @@
-package test;
+public class SamsungX extends TV implements IRemoteControl{
 
-public class SamsungX {
+	public SamsungX(int s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
 
+	@Override
+	public void changeChannel(int channel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void increaseVol(int volume) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decreaseVol(int volume) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean powerOn() {
+		if(!this.isOn()) {
+			System.out.println("SamsungX");
+			this.setOn(true);
+		}
+		return isOn();
+	}
+
+	@Override
+	public boolean powerOff() {
+		if(this.isOn()) {
+			System.out.println("SamsungX");
+			this.setOn(false);
+		}
+		return isOn();
+	}
 }
