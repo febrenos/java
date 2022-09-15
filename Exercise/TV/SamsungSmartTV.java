3cc4ef1d200055948ede82c524b6854d2b7d5998
Fe <54954812+febreno@users.noreply.github.com>
2022-09-15 13:59:00 -0300

    Update SamsungSmartTV.java

--- Exercise/TV/SamsungSmartTV.java
+++ Exercise/TV/SamsungSmartTV.java
@@ -3,8 +3,8 @@ package test;
 public class SamsungSmartTV extends TV implements IRemoteControl {
 
 	//final ins constant(inalteravel)
-	public final String MODEL = "TV001";
-	
+	public final String MODEL = "SamsungSmartTV001";
+		
 	public SamsungSmartTV(int s) {
 		super(s);
 		// TODO Auto-generated constructor stub
@@ -12,32 +12,35 @@ public class SamsungSmartTV extends TV implements IRemoteControl {
 
 	@Override
 	public void changeChannel(int channel) {
-		// TODO Auto-generated method stub
-		
+	// TODO Auto-generated method stub
+			
 	}
 
 	@Override
 	public void increaseVol(int volume) {
 		// TODO Auto-generated method stub
-		
+			
 	}
 
 	@Override
 	public void decreaseVol(int volume) {
 		// TODO Auto-generated method stub
-		
+			
 	}
 
 	@Override
 	public boolean powerOn() {
-		// TODO Auto-generated method stub
-		return false;
+		if(!this.isOn()) {
+			this.setOn(true);
+		}
+		return isOn();
 	}
 
 	@Override
 	public boolean powerOff() {
-		// TODO Auto-generated method stub
-		return false;
+		if(this.isOn()) {
+			this.setOn(false);
+		}
+		return isOn();
 	}
-	
 }
