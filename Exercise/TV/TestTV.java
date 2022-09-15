c2f973918b986edef60301c944021b7e4e127c86
Fe <54954812+febreno@users.noreply.github.com>
2022-09-15 14:00:08 -0300

    Update TestTV.java

--- Exercise/TV/TestTV.java
+++ Exercise/TV/TestTV.java
@@ -1,5 +1,14 @@
 package test;
 
 public class TestTV {
-
+	public static void main(String[] args) {
+		SamsungX tv1 = new SamsungX(0);
+		SamsungSmartTV tv2 = new SamsungSmartTV(0);
+		
+		tv1.powerOn();
+		tv2.changeChannel(5);
+		
+		tv2.powerOn();
+		tv1.changeChannel(5);
+	}
 }
